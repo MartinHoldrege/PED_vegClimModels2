f28b95cd795140e264b06acf8dd2829510d530a4
martinholdrege <martinholdrege@gmail.com>
2026-05-15 20:33:06 +0000

    working version of app w/ ecdf plot

--- Analysis/BiomassQuantity/Evaluate/01_app_rap-tree-cover.js
+++ Analysis/BiomassQuantity/Evaluate/01_app_rap-tree-cover.js
@@ -3,6 +3,7 @@ App for exploring tree cover at native 30m resolution.
 Supports RAP and USFS tree canopy cover datasets.
 Optional masking by fire (MTBS) and land cover (LCMAP).
 Split panel with independent controls on each side.
+CDF chart compares left and right panel datasets.
 
 Author: Martin Holdrege
 Started: April 2026
@@ -17,17 +18,15 @@ var yearEnd = 2023;
 
 // data ---------------------------------------------
 
-// RAP tree cover (mean 2019-2023)
 var rapTree = ee.ImageCollection('projects/rap-data-365417/assets/vegetation-cover-v3')
   .filter(ee.Filter.calendarRange(yearStart, yearEnd, 'year'))
   .select(['TRE'])
   .mean();
 
-// USFS tree canopy cover (mean 2019-2023)
 var usfsTree = ee.ImageCollection('USGS/NLCD_RELEASES/2023_REL/TCC/v2023-5')
   .filter(ee.Filter.calendarRange(yearStart, yearEnd, 'year'))
   .filter('study_area == "CONUS"')
-  .select(['Science_Percent_Tree_Canopy_Cover'])
+  .select(['Science_Percent_Tree_Canopy_Cover', 'NLCD_Percent_Tree_Canopy_Cover'])
   .mean();
 
 var everBurned = ee.Image(fg.pathAsset + 'fire/MTBS_everBurned_30m_2000-2023')
@@ -35,28 +34,16 @@ var everBurned = ee.Image(fg.pathAsset + 'fire/MTBS_everBurned_30m_2000-2023')
 
 var datasets = {
   'RAP tree cover': rapTree,
-  'USFS tree canopy cover': usfsTree
+  'USFS tree canopy cover (Science)': usfsTree.select('Science_Percent_Tree_Canopy_Cover'),
+  'USFS tree canopy cover (NLCD)': usfsTree.select('NLCD_Percent_Tree_Canopy_Cover')
 };
 var datasetNames = Object.keys(datasets);
 
 // viz --------------
-
 var paletteCov = [
-    'CDA066',
-    'D7C29E',
-    'C2D096',
-    'B7D692',
-    'ADDD8E',
-    '78C679',
-    '5CB86B',
-    '41AB5D',
-    '39A156',
-    '329750',
-    '238443',
-    '11763D',
-    '006837',
-    '004529'
-  ]
+  'CDA066', 'D7C29E', 'C2D096', 'B7D692', 'ADDD8E', '78C679', '5CB86B',
+  '41AB5D', '39A156', '329750', '238443', '11763D', '006837', '004529'
+];
 
 // defaults -----------------------------------------
 var defaults = {
@@ -68,7 +55,92 @@ var defaults = {
   dataset: datasetNames[0]
 };
 
-// functions ----------------------------------------
+// shared state -------------------------------------
+var leftVals = null;
+var rightVals = null;
+var leftMap = ui.Map();
+var rightMap = ui.Map();
+var chartPanel = null;
+
+// chart helpers ------------------------------------
+
+var getMasked = function(vals) {
+  var img = datasets[vals.dataset];
+  var mask = ee.Image(1);
+  if (vals.maskFire) mask = mask.and(everBurned.not());
+  if (vals.maskLcmap) mask = mask.and(fg.lcmapMask);
+  return img.updateMask(mask);
+};
+
+var parseResult = function(result) {
+  var values = [];
+  var keys = Object.keys(result);
+  var prefix = keys[0].split('_p')[0]; // e.g. 'TRE'
+  for (var i = 0; i <= 100; i++) {
+    var key = prefix + '_p' + i;
+    values.push(result[key] !== undefined ? result[key] : null);
+  }
+  return values;
+};
+
+var updateChart = function() {
+  if (chartPanel) {
+    leftMap.remove(chartPanel);
+  }
+  chartPanel = ui.Panel({
+    style: {position: 'bottom-left', width: '350px', height: '250px',
+      backgroundColor: 'rgba(255,255,255,0.9)'}
+  });
+  leftMap.add(chartPanel);
+  chartPanel.add(ui.Label('Computing ...', {fontSize: '10px'}));
+
+  var pctList = ee.List.sequence(0, 100, 1);
+  var region = leftMap.getBounds(true);
+  var leftMasked = getMasked(leftVals);
+  var rightMasked = getMasked(rightVals);
+
+  var leftPct = leftMasked.reduceRegion({
+    reducer: ee.Reducer.percentile(pctList),
+    geometry: region, scale: 250, maxPixels: 1e8,
+    bestEffort: true
+  });
+  var rightPct = rightMasked.reduceRegion({
+    reducer: ee.Reducer.percentile(pctList),
+    geometry: region, scale: 250, maxPixels: 1e8,
+    bestEffort: true
+  });
+
+  ee.Dictionary(leftPct).evaluate(function(leftResult) {
+    ee.Dictionary(rightPct).evaluate(function(rightResult) {
+      if (!leftResult || !rightResult) {
+        chartPanel.clear();
+        chartPanel.add(ui.Label('No data in view', {fontSize: '10px'}));
+        return;
+      }
+      var leftY = parseResult(leftResult);
+      var rightY = parseResult(rightResult);
+      var rows = [];
+      for (var i = 0; i <= 100; i++) {
+        rows.push([i, leftY[i], rightY[i]]);
+      }
+      var dataTable = [
+        ['Percentile', 'Left: ' + leftVals.dataset, 'Right: ' + rightVals.dataset]
+      ].concat(rows);
+      var chart = ui.Chart(dataTable)
+        .setChartType('LineChart')
+        .setOptions({
+          title: 'Tree cover CDF (~ >= 250m, current view)',
+          hAxis: {title: 'Percentile'},
+          vAxis: {title: 'Tree cover (%)'},
+          lineWidth: 2,
+          colors: ['blue', 'red']
+        });
+      chartPanel.clear();
+      chartPanel.add(chart);
+    });
+  });
+};
+// build panel function -----------------------------
 
 var buildPanel = function(map, position) {
   var vals = {
@@ -79,6 +151,12 @@ var buildPanel = function(map, position) {
     maskLcmap: defaults.maskLcmap,
     dataset: defaults.dataset
   };
+
+  if (position === 'top-left') {
+    leftVals = vals;
+  } else {
+    rightVals = vals;
+  }
 
   var updateMap = function() {
     var img = datasets[vals.dataset];
@@ -97,7 +175,7 @@ var buildPanel = function(map, position) {
       .blend(ee.Image(1).updateMask(mask).updateMask(img.gte(vals.maxTree)));
 
     var viz = {min: 0, max: vals.colorMax, palette: paletteCov};
-    
+
     map.layers().reset([
       ui.Map.Layer(outOfRange, {min: 0, max: 1, palette: ['black', 'gray']},
         'outside range (' + vals.minTree + '–' + vals.maxTree + '%)'),
@@ -150,19 +228,30 @@ var buildPanel = function(map, position) {
     }
   });
 
+  var widgets = [
+    ui.Label('Tree cover ' + yearStart + '–' + yearEnd,
+      {fontWeight: 'bold', fontSize: '12px'}),
+    ui.Label('Dataset:', {fontSize: '11px', margin: '4px 0px 0px 0px'}),
+    dropdown,
+    makeSlider('Min tree cover (%; below is black)', 0, 100, 1, defaults.minTree, 'minTree'),
+    makeSlider('Max tree cover (%; above is gray)', 0, 100, 1, defaults.maxTree, 'maxTree'),
+    makeSlider('Color saturation (%)', 1, 100, 1, defaults.colorMax, 'colorMax'),
+    ui.Label('Masking', {fontWeight: 'bold', fontSize: '11px', margin: '8px 0px 2px 0px'}),
+    maskFireCheck,
+    maskLcmapCheck
+  ];
+
+  if (position === 'top-right') {
+    var chartButton = ui.Button({
+      label: 'Show ECDF (current view, \nbased on pixels in current view)',
+      style: {stretch: 'horizontal'},
+      onClick: updateChart
+    });
+    widgets.push(chartButton);
+  }
+
   var panel = ui.Panel({
-    widgets: [
-      ui.Label('Tree cover ' + yearStart + '–' + yearEnd,
-        {fontWeight: 'bold', fontSize: '12px'}),
-      ui.Label('Dataset:', {fontSize: '11px', margin: '4px 0px 0px 0px'}),
-      dropdown,
-      makeSlider('Min tree cover (%; below is black)', 0, 100, 1, defaults.minTree, 'minTree'),
-      makeSlider('Max tree cover (%; above is gray)', 0, 100, 1, defaults.maxTree, 'maxTree'),
-      makeSlider('Color saturation (%)', 1, 100, 1, defaults.colorMax, 'colorMax'),
-      ui.Label('Masking', {fontWeight: 'bold', fontSize: '11px', margin: '8px 0px 2px 0px'}),
-      maskFireCheck,
-      maskLcmapCheck
-    ],
+    widgets: widgets,
     style: {position: position, padding: '4px', width: '260px'}
   });
 
@@ -173,8 +262,6 @@ var buildPanel = function(map, position) {
 // build app ----------------------------------------
 ui.root.clear();
 
-var leftMap = ui.Map();
-var rightMap = ui.Map();
 leftMap.setControlVisibility(true);
 rightMap.setControlVisibility(true);
 
@@ -202,4 +289,4 @@ var infoPanel = ui.Panel({
   style: {position: 'bottom-right', padding: '4px',
     backgroundColor: 'rgba(255,255,255,0.8)'}
 });
-rightMap.add(infoPanel);
+leftMap.add(infoPanel);
