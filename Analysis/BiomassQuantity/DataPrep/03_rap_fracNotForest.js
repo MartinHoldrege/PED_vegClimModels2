e6445a73df55c2c6c76f5b39588e0c8b0241b7ea
martinholdrege <martinholdrege@gmail.com>
2026-05-18 14:24:18 +0000

    updated to loop through notree cutoffs

--- Analysis/BiomassQuantity/DataPrep/03_rap_fracNotForest.js
+++ Analysis/BiomassQuantity/DataPrep/03_rap_fracNotForest.js
@@ -13,38 +13,43 @@ var fg = require('users/MartinHoldrege/PED_vegClimModels2:Functions/gee/general.
 var yearStartRap = 2019;
 var yearEndRap = 2023;
 
-// read in data -------------------------------------
-// created in 03_rap_notForest.js (masked by fire + LCMAP)
-var notForest30 = ee.Image(fg.pathAsset + 'rap/RAP_v3_tree-lt1_masked_' +
-  yearStartRap + '-' + yearEndRap + '_30m');
+var cutoffs = [3, 5, 10];
 
-// process ------------------------------------------
-var fracNotForest = notForest30
-  .reduceResolution({
-    reducer: ee.Reducer.mean(),
-    bestEffort: true,
-    maxPixels: 2e3
-  })
-  .reproject({
+for (var i = 0; i < cutoffs.length; i++) {
+  var cutoff = cutoffs[i];
+  // read in data -------------------------------------
+  // created in 03_rap_notForest.js (masked by fire + LCMAP)
+  var notForest30 = ee.Image(fg.pathAsset + 'rap/RAP_v3_tree-lt' + cutoff + '_masked_' +
+    yearStartRap + '-' + yearEndRap + '_30m');
+  
+  // process ------------------------------------------
+  var fracNotForest = notForest30
+    .reduceResolution({
+      reducer: ee.Reducer.mean(),
+      bestEffort: true,
+      maxPixels: 2e3
+    })
+    .reproject({
+      crs: fg.crs,
+      crsTransform: fg.crsTransform
+    })
+    .rename('fracNotForest');
+  
+  // visualize ----------------------------------------
+  Map.addLayer(fracNotForest, {min: 0, max: 1, palette: ['white', 'black']},
+    'frac not forest (unburned, natural land)', false);
+  
+  // export -------------------------------------------
+  var fileName = 'RAP_v3_fracNotForest_lt' + cutoff + '_' +
+    yearStartRap + '-' + yearEndRap + fg.resLabel;
+  
+  Export.image.toAsset({
+    image: fracNotForest,
+    description: fileName,
+    assetId: fg.pathAsset + 'rap/' + fileName,
     crs: fg.crs,
-    crsTransform: fg.crsTransform
-  })
-  .rename('fracNotForest');
-
-// visualize ----------------------------------------
-Map.addLayer(fracNotForest, {min: 0, max: 1, palette: ['white', 'black']},
-  'frac not forest (unburned, natural land)', false);
-
-// export -------------------------------------------
-var fileName = 'RAP_v3_fracNotForest_' +
-  yearStartRap + '-' + yearEndRap + fg.resLabel;
-
-Export.image.toAsset({
-  image: fracNotForest,
-  description: fileName,
-  assetId: fg.pathAsset + 'rap/' + fileName,
-  crs: fg.crs,
-  crsTransform: fg.crsTransform,
-  region: fg.region,
-  maxPixels: 1e12
-});
+    crsTransform: fg.crsTransform,
+    region: fg.region,
+    maxPixels: 1e12
+  });
+}
