3d434de4b8c15d80a7a84f6267cbee75af6182b2
martinholdrege <martinholdrege@gmail.com>
2026-08-12 16:37:58 +0000

    Updated Analysis/Cover/DataPrep/01_create_lcmap_mask.js (added 9 lines, deleted 38 lines).

--- Analysis/Cover/DataPrep/01_create_lcmap_mask.js
+++ Analysis/Cover/DataPrep/01_create_lcmap_mask.js
@@ -12,35 +12,11 @@ Script started: April 3, 2026
 var fg = require('users/MartinHoldrege/PED_vegClimModels2:Functions/gee/general.js');
 
 // read in data -----------------------------
-var lcpri = ee.ImageCollection("projects/sat-io/open-datasets/LCMAP/LCPRI");
 
-// process --------------------------------------------------------------
-
-var lcmap2021 = lcpri
-  .filterDate('2021-01-01', '2021-12-31')
-  .first();
-
-
-var vizLcpri = {
-  min: 1,
-  max: 8,
-  palette: [
-    'e81414',  // 1 - Developed
-    'b68c4c',  // 2 - Cropland
-    'd2cda4',  // 3 - Grass/Shrub
-    '38814e',  // 4 - Tree Cover
-    '4bb0d4',  // 5 - Water
-    '85c8c8',  // 6 - Wetlands
-    'eeeeee',  // 7 - Ice/Snow
-    'b5a27e'   // 8 - Barren
-  ]
-};
-Map.addLayer(lcmap2021, vizLcpri, 'lcmap');
 // Binary mask: 1 = keep (grass/shrub, tree, wetlands, ice/snow, barren), 0 = remove (developed, cropland, water)
-var mask = lcmap2021.remap(
-  [1, 2, 3, 4, 5, 6, 7, 8],
-  [0, 0, 1, 1, 0, 1, 1, 1]
-);
+var mask = fg.lcmapMask; // at native resolution
+
+// process ------------------------------------------
 
 // Fraction of "keep" pixels within each Daymet cell
 
@@ -58,14 +34,9 @@ var fracKeep = mask
 var fracKeep = fracKeep.rename('fracKeep');
 // Map.addLayer(fracKeep, {min: 0, max: 1, palette: ['red', 'white', 'green']}, 'fraction keep');
 
-Map.addLayer(fracKeep.geometry(), {}, '')
+Map.addLayer(fracKeep.geometry(), {}, '');
 
-Export.image.toAsset({
-  image: fracKeep,
-  description: 'LCMAP_fracKeep_daymet',
-  assetId: 'projects/ee-martinholdrege/assets/PED_vegClimModels2/LCMAP_fracKeep_daymet',
-  crs: fg.crs,
-  crsTransform: fg.crsTransform,
-  region: fracKeep.geometry(), // or define a CONUS geometry
-  maxPixels: 1e12
-});
+fg.exportAsset(fracKeep, 'LCMAP_fracKeep_daymet' + fg.resLabel, 'masks/');
+
+
+
